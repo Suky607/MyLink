22d56d7bd7445851ba4b894b9ad58d1f4f12b68f
Suky607 <1970724489@qq.com>
2018-10-31 20:05:18 +0800

    更改

--- homework5/js/test2.js
+++ homework5/js/test2.js
@@ -13,8 +13,8 @@
 var imagesA=document.getElementById("images").children;
 
 //获取一组文本
-var txtList=document.querySelectorAll(".txt-box>li")
-
+// var txtList=document.querySelectorAll(".txt-box>li")
+var txtList = document.querySelector(".txt-box").children;
 
 
 // console.log(imagesA);
@@ -27,44 +27,74 @@ var txtList=document.querySelectorAll(".txt-box>li")
 // imagesA[4].style.display="block";
 
 //利用计时器间隔1s,显示1张图像，其余图像隐藏
+//初始化当前显示的图片/文本编号
 var currentNo=0;
+const nodeLength=8;
+
+//计时器换片函数，间隔1S被调用,显示1张图像，其余图像隐藏。文本轮流高亮
 function changeImg()
 {
 //排他原理，先去掉同类，再突出自己
-// for(var i=0;i<imagesA.length;i++)
-// {
-//     imagesA[i].className="hiddenImg";
-//     txtList[i].className="txtItem normalColor";
+for(var i=0;i<nodeLength;i++)
+ {
+     // 同类图片透明度0（.hiddenImg)
+    imagesA[i].className="hiddenImg";
+    //同类文本设置正常非高亮
+    txtList[i].className="txtItem normalColor";
 
   
  
 
-    //再突出自己
-    // imagesA[currentNo].className="displayImg";
-    // txtList[currentNo].className="txtItem heighlightColor";
-
+    //再突出自己,当前图片透明度1(.displayImg)
+     imagesA[currentNo].className="displayImg";
+     txtList[currentNo].className="txtItem heighlightColor";
+ }
+ }
     //换个元素，为下一个计时器调用做准备
-    if(currentNo<7){currentNo++;}
-    else{currentNo=0;}
+    // if(currentNo<7){currentNo++;}
+    // else{currentNo=0;}
 
-     console.log(currentNo);
+    //  console.log(currentNo);
+    function leftImg()
+    {
+        if(currentNo>0)
+        {
+            currentNo--;
+        }
+        else{
+            currentNo=7;
+        }
+    }
+    function rightImg()
+    {
+        if(currentNo<7)
+        {
+            currentNo++;
+        }
+        else{
+            currentNo=0;
+        }
+    }
 
-}
-var timer=window.setInterval(changeImg,1000)
+//网页加载后启动定时器，每隔1秒调用changeImgGo（）换片
+var timer=window.setInterval(rightImgGo,1000)
 
 //定时器移入移出暂停启动按钮
-
+//鼠标移出后移除定时器
 function stopChange()
 {
     window.clearInterval(timer)
 }
+//鼠标移入后重设定时器
 function startChange()
 {
-    timer=window.setInterval(changeImg,1000);
+    timer=window.setInterval(rightImgGo,1000);
 }
+//获取sliderDIV以注册移入移出事件
 var sliderDiv=document.querySelector(".slider");
 
 // console.log(sliderDiv);
+//为sliderDIV注册移入移出事件
 sliderDiv.addEventListener('mouseover',stopChange);
 sliderDiv.addEventListener('mouseout',startChange);
 
@@ -89,41 +119,16 @@ function gotoImg()
 var leftButton=document.querySelector('.leftButton');
 var rigetButton=document.querySelector('.rigetButton');
 
-leftButton.addEventListener('click',leftImg);
-rigetButton.addEventListener('click',rightImg);
+leftButton.addEventListener('click',leftImgGo);
+rigetButton.addEventListener('click',rightImgGo);
 
 
-function leftImg(){
-
-    for(var i=0;i<imagesA.length;i++)
-    {
-        imagesA[i].className="hiddenImg";
-        txtList[i].className="txtItem normalColor";
-  
-      //   console.log(imagesA[i]);
-    }
-    if(currentNo>0)
-    {
-        currentNo--;
-    }
-    else
-    {
-        currentNo=7;
-    }
-    imagesA[currentNo].className="displayImg";
-    txtList[currentNo].className="txtItem heighlightColor";
-
+function leftImgGo(){
+    leftImg();
+    changeImg();
 }
-function rightImg()
-{
-    if(currentNo<7)
-    {
-        currentNo++;
-    }
-    else
-    {
-        currentNo=0;
-    }
-    console.log(currentNo);
+
+function rightImgGo(){
+    rightImg();
     changeImg();
 }
